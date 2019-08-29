362ebf13f96aab9b9e3c1d6d9ebb72c5feee9b94
chenyunpet <54629863+chenyunpet@users.noreply.github.com>
2019-08-29 17:30:12 -0500

    代码上传
    1

---
+++ Script/AnimSet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract  class AnimSet:MonoBehaviour
+{
+    public abstract string GetIdleAnim();
+    public abstract string GetMoveAnim(E_MotionType motion, E_MoveType move, E_WeaponType weapon, E_WeaponState weaponState);
+    public abstract string GetShowWeaponAnim();
+    public abstract string GetHideWeaponAnim();
+}
+
